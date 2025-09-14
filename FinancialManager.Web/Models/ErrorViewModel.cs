0f2e2a7e82e624b3a8d74a6e55cdb2228d0f64c1
bubble-bobble <joanrojasp@gmail.com>
2025-09-14 10:06:10 -0300

    Initial commit

---
+++ FinancialManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinancialManager.Web.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
